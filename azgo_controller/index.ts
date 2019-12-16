5b75b5290f20001f289e8fa0934c259d1a7f2a7c
jjcollinge <jonathancollinge@live.com>
2019-12-16 10:42:09 +0000

    fixed trigger script

--- azgo_controller/index.ts
+++ azgo_controller/index.ts
@@ -5,23 +5,36 @@ import tmp = require('tmp');
 import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 import * as msRestNodeAuth from "@azure/ms-rest-nodeauth";
-import { ResourceManagementClient, ResourceManagementModels, ResourceManagementMappers } from "@azure/arm-resources";
+import { ResourceManagementClient, ResourceManagementModels, ResourceManagementMappers, ResourceGroups, Deployments } from "@azure/arm-resources";
+import glob from "glob"
+import mergeARMTemplates from "arm-template-merge"
+import { Resource } from "@azure/arm-resources/esm/models/mappers";
+import * as fs from "fs";
 
 const subscriptionId = process.env["AZURE_SUBSCRIPTION_ID"];
-const tenantId = process.env["AZURE_TENANT_ID"]
-const clientSecret = process.env["AZURE_CLIENT_ID"]
-const clientId = process.env["AZURE_CLIENT_SECRET"]
+const tenantId = process.env["AZURE_TENANT_ID"];
+const clientSecret = process.env["AZURE_CLIENT_ID"];
+const clientId = process.env["AZURE_CLIENT_SECRET"];
+const resourecGroupName = process.env["AZGO_RESOURCE_GROUP"];
+const resourecGroupLocation = process.env["AZGO_RESOURCE_GROUP_LOCATION"];
 
-async function CloneRepoAsync(git: simplegit.SimpleGit, cloneURL: string) {
+async function CloneRepoAsync(git: simplegit.SimpleGit, cloneURL: string): Promise<string> {
     var tmpDir = tmp.dirSync();
     await git.clone(cloneURL, tmpDir);
     return tmpDir.name;
 }
 
+function GetARMTemplatePaths(root: string): Array<String> {
+    if (root[root.length - 1] == "/") {
+        root = root.slice(0, root.length - 1);
+    }
+    return glob.sync(root + "/**.json")
+}
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('azgo controller triggered.');
 
-    // request validation
+    // TODO: request validation
 
     let event: PushEvent = JSON.parse(req.body);
     context.log('commit: ' + event.head_commit);
@@ -30,10 +43,55 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     let git: simplegit.SimpleGit = simplegit();
     let headDir = await CloneRepoAsync(git, 
         event.repository.clone_url.replace("{/sha}", event.head_commit));
+    context.log("cloned repo " + event.repository.name + " to directory " + headDir);
 
     // filter ARM templates
+    let armTemplatePaths = GetARMTemplatePaths(headDir);
+    let numARMTemplatesFound = armTemplatePaths.length;
+
+    if (numARMTemplatesFound < 1) {
+        context.log("no ARM templates found");
+        return context.done();
+    } else {
+        context.log("found ARM " + numARMTemplatesFound + " templates")
+    }
+
+    // merge into single ARM template
+    let merged = {};
+    armTemplatePaths.forEach(armTemplatePath => {
+        let armTemplate = fs.readFileSync(armTemplatePath as string, 'utf8')
+        merged = mergeARMTemplates(armTemplate);
+    });
 
     // apply update to ARM
+    let creds = await msRestNodeAuth.loginWithServicePrincipalSecret(clientId, clientSecret, tenantId);
+    let client = new ResourceManagementClient(creds, subscriptionId);
+    let deployment: ResourceManagementModels.Deployment = {
+        properties: {
+            mode: "Complete",
+            template: merged
+        }
+    };
+
+    let resourceGroupExists = await client.resourceGroups.checkExistence(resourecGroupName);
+    if (resourceGroupExists._response.bodyAsText == "false") {
+        context.log("resource group " + resourecGroupName + "does not exist, creating it");
+        let resourceGroup = {
+            name: resourecGroupName,
+            location: resourecGroupLocation
+        };
+        await client.resourceGroups.createOrUpdate(resourecGroupName, resourceGroup);
+    }
+    
+    let res = await client.deployments.createOrUpdate(resourecGroupName, resourecGroupName, deployment);
+    if (res._response.status == 200) {
+        context.log("successful deployment");
+    } else {
+        context.log("unsuccessful deployment, return http status code: " + res._response.status);
+        context.log("error message: " + res._response.bodyAsText);
+    }
+
+    return context.done();
 };
 
 export default httpTrigger;
--- scripts/trigger.sh
+++ scripts/trigger.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-const GitHubApiEndpoint = "https://api.github.com/repos"
+readonly github_api_endpoint="https://api.github.com"
+
+function ensure_installed {
+    if ! [ -x "$(command -v $1)" ]; then
+        echo "$1 is not installed but is required, please fix and retry"
+        exit 1
+    fi
+}
 
 function ensure_required {
     if [ -z "$1" ]; then
@@ -10,21 +17,29 @@ function ensure_required {
 }
 
 function trigger_webhook {
-    let GitHubApiWebhookTestEndpoint = "${GitHubApiEndpoint}/repos/${1}/${2}/hooks/${3}/tests"
+    readonly github_api_webhook_test_endpoint="${github_api_endpoint}/repos/${1}/${2}/hooks/${3}/tests"
 
-    echo "request: POST ${GitHubApiWebhookTestEndpoint}"
-    status_code=$(curl -u ${1}:${4} --write-out "%{http_code}\n" --silent --output /dev/null -X POST "${GitHubApiWebhookTestEndpoint}")
+    if [ -z "$AZGO_DEBUG" ]; then
+        curl_args="--silent --output /dev/null"
+        err_prompt="Retry with 'AZGO_DEBUG=true'"
+    else
+        curl_args="-v"
+    fi
+
+    http_status_code=$(curl -u "${1}:${4}" --write-out "%{http_code}\n" ${curl_args} -X POST "${github_api_webhook_test_endpoint}")
     
-    if [ "$status_code" -eq "204" ]; then
+    if [[ "$http_status_code" == "204" ]]; then
         echo "Successfully invoked push webhook trigger"
     else
-        echo "Error invoking webhook. Status code: $status_code"
+        echo "Error invoking webhook. Status code: $http_status_code. $err_prompt"
         exit 1
     fi
 }
 
 function main {
     source .env
+
+    ensure_installed jq
 
     ensure_required "$AZGO_REPO_OWNER" "\$AZGO_REPO_OWNER"
     ensure_required "$AZGO_REPO" "\$AZGO_REPO"
